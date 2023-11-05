5efeb22dded49bdb08f9e85c2e046dd49fa4871f
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-05 14:28:24 +0900

    chore: add query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Router as RemixRouter } from '@remix-run/router';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 
 import { router as browserRouter } from './router';
@@ -9,10 +10,12 @@ interface Props {
 }
 
 const App = ({ router = browserRouter }: Props) => {
+  const client = useMemo(() => new QueryClient(), []);
+
   return (
-    <>
+    <QueryClientProvider client={client}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 };
 
