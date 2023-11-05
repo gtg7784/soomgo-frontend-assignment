cec8d6506976738da28a47ba008bc01bf26f32c5
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-05 23:08:18 +0900

    chore: change `react-query` import as `@tanstack/react-query`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Router as RemixRouter } from '@remix-run/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useMemo } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 
 import { router as browserRouter } from './router';
