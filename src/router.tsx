072a62aaaba9cc2cf6540fc3d24d5077b3f456b7
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-05 23:09:03 +0900

    feat: apply `Layout` component to all routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,19 @@
 import { RouteObject, createBrowserRouter } from 'react-router-dom';
 
+import Layout from './components/layout';
 import IndexPage from './pages';
 import RequestFormPage from './pages/request-form';
 
 export const routes: RouteObject[] = [
   {
     path: '/',
-    element: <IndexPage />,
+    element: <Layout />,
     errorElement: <div>404</div>,
     children: [
+      {
+        path: '/',
+        element: <IndexPage />,
+      },
       {
         path: '/request-form',
         element: <RequestFormPage />,
