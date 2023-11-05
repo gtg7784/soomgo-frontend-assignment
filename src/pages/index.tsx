1b477287efa11b6c52dca12608b30ba1978d576a
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-05 23:37:29 +0900

    feat(page): add 요청서 index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const IndexPage = () => {
   return (
-    <div>
-      <h1>요청서</h1>
+    <div className="flex flex-col h-full w-full p-8">
+      <h1 className="text-4xl font-extrabold">요청서</h1>
+      <ul className="mt-6">
+        <li>
+          <Link to="/request-form?id=1" className="underline">
+            대청소
+          </Link>
+        </li>
+        <li>
+          <Link to="/request-form?id=2" className="underline">
+            영어 과외
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
