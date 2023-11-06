3123d4a8fe40621de0e243200dbe28bc9700db0c
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-07 01:38:57 +0900

    feat(component): update width, height of layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="bg-white min-h-screen min-w-screen relative">
+    <div className="bg-white h-screen w-screen relative">
       <Outlet />
     </div>
   );
