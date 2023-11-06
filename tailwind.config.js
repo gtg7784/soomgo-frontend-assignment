399a78927fa2be278c26c21411a1d793b51f2d9c
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-07 01:39:22 +0900

    chore(tailwind): add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+      },
+    },
   },
   plugins: [require('tailwindcss-animate')],
 };
