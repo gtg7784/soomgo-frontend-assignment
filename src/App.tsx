5a48d1181d43f4d5d11fde4581172a0ad3b923df
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2023-11-07 11:08:31 +0900

    chore: add ReduxProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Router as RemixRouter } from '@remix-run/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useMemo } from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { router as browserRouter } from './router';
+import store from './store';
 
 interface Props {
   router?: RemixRouter;
@@ -14,7 +16,9 @@ const App = ({ router = browserRouter }: Props) => {
 
   return (
     <QueryClientProvider client={client}>
-      <RouterProvider router={router} />
+      <ReduxProvider store={store}>
+        <RouterProvider router={router} />
+      </ReduxProvider>
     </QueryClientProvider>
   );
 };
